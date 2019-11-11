213326debf775bb5c97816b2f63a4f374056b987
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-11 14:28:21 +0000

    Stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'questions#ask'
+  get '/ask(.:format)', to: 'questions#ask', as: :ask
+  get '/answer(.:format)', to: 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
